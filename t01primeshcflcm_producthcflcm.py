699a0e9d88be28c2f02f39b3dc5a06c021a6901a
chibai-wot <52991942+chibai-wot@users.noreply.github.com>
2019-10-24 14:56:20 +0800

    Update t01primeshcflcm_producthcflcm.py

--- t01primeshcflcm_producthcflcm.py
+++ t01primeshcflcm_producthcflcm.py
@@ -1,5 +1,5 @@
 # Verify that the product of 2 numbers is equal to the product of their HCF and their LCM.
 
-num1 = 
-num2 = 
+num1 = num1(HCF) * num1(LCM) / num2
+num2 = num2(HCF) * num2(LCM) / num1
 
